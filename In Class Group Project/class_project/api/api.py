04698e9688715c4e7ef9bb2a09d5c08a3df5879f
Kay Ashaolu <kayashaolu@berkeley.edu>
2023-04-27 15:21:37 -0700

    final in class commit

--- In Class Group Project/class_project/api/api.py
+++ In Class Group Project/class_project/api/api.py
@@ -16,66 +16,50 @@ def filtered_data():
     end = request.args.get("end")
     states = request.args.get("states")
     types = request.args.get("types")
-    low_amt = request.args.get('low_amt', 0)
-    high_amt = request.args.get('high_amt', 0)
+    low_amt = request.args.get('low_amt', "0")
+    high_amt = request.args.get('high_amt', "0")
 
-    start_sql = datetime.date(start.split("/")[1], start.split("/")[0], 1)
-    end_sql = datetime.date(end.split("/")[1], end.split("/")[0], 1)
-    states_sql = states.split(",")
-    types_sql = types.split(",")
-    low_amt_sql = low_amt
-    high_amt_sql = high_amt
+    start_sql = datetime.date(int(start.split("/")[1]), int(start.split("/")[0]), 1)
+    end_sql = datetime.date(int(end.split("/")[1]), int(end.split("/")[0]), 1)
+    states_sql = tuple(states.split(","))
+    types_sql = tuple(types.split(","))
+    low_amt_sql = int(low_amt)
+    high_amt_sql = int(high_amt)
 
 
     stmt_sql = """ SELECT state, type, amount, date
                     FROM energy_consumption
                     WHERE date BETWEEN :start_sql AND :end_sql
                     AND state IN :states_sql
-                    AND types IN :types_sql
+                    AND type IN :types_sql
     """
 
-    if low_amt != 0 or high_amt != 0:
+    if low_amt_sql != 0 or high_amt_sql != 0:
         stmt_sql += "AND amount BETWEEN :low_amt_sql AND :high_amt_sql"
 
     stmt = text(stmt_sql)
-    stmt = stmt.bindparams()
 
-    stmt = text("SELECT quote FROM quotes WHERE day_of_week = :x")
-        stmt = stmt.bindparams(x=day_of_week)
-        result = db.session.execute(stmt).all()
-
-
-
-def gen_response(day_of_week):
-    if not day_of_week:
-        response = {"message": "We need the day_of_week in order to send a quote"}
-        response_code = 400
+    if low_amt_sql != 0 or high_amt_sql != 0:
+        stmt = stmt.bindparams(start_sql=start_sql, 
+                            end_sql=end_sql,
+                            states_sql=states_sql,
+                            types_sql=types_sql,
+                            low_amt_sql=low_amt_sql,
+                            high_amt_sql=high_amt_sql)
     else:
-        stmt = text("SELECT quote FROM quotes WHERE day_of_week = :x")
-        stmt = stmt.bindparams(x=day_of_week)
-        result = db.session.execute(stmt).all()
+        stmt = stmt.bindparams(start_sql=start_sql, 
+                            end_sql=end_sql,
+                            states_sql=states_sql,
+                            types_sql=types_sql)
 
-        if not result:
-             response = {"message": "Sorry we don't know that day of the week" }
-             response_code = 404
-        else:
-            response = {"day": day_of_week, "quote": result[0].quote}
-            response_code = 200
+    result = db.session.execute(stmt).all()
 
-    return json.dumps(response), response_code
+    result_json = {}
+    result_rows = []
 
-@app.route('/quote/<day_of_week>')
-def quote_of_the_day(day_of_week):
-    return gen_response(day_of_week)
+    for row in result:
+        current_row_data = {"state": row[0], "type": row[1], "amount": row[2], "date": row[3].strftime("%m/%y")}
+        result_rows.append(current_row_data)
 
-@app.route('/quote', methods = ['GET'])
-@app.route('/quote/', methods = ['GET'])
-def quote_of_the_day_get():
-    day_of_week = request.args.get("day_of_week")
-    return gen_response(day_of_week)
-
-@app.route('/quote', methods = ['POST'])
-def quote_of_the_day_post():
-    data = request.get_json()
-    day_of_week = data.get("day_of_week")
-    return gen_response(day_of_week)
+    result_json["results"] = result_rows
+    return json.dumps(result_json)
